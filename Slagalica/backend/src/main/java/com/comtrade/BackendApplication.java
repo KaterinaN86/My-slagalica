7d4a08ad6f60d19b48090499c0b419387683b359
Mirko Petrovic <66567615+miirko99@users.noreply.github.com>
2022-11-16 15:56:05 +0100

    Uklonjen visak

--- Slagalica/backend/src/main/java/com/comtrade/BackendApplication.java
+++ Slagalica/backend/src/main/java/com/comtrade/BackendApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = UserRepository.class)
-@EntityScan("com.comtrade.*")
 public class BackendApplication {
 
 	public static void main(String[] args) {
