e22b0f1cd568c9b6420c906e32a5d7926f388d59
Benjamin Grbo <benjamin.grbo@fsk.unsa.ba>
2022-11-23 12:48:46 +0100

    deleted slagalica subfolder

--- Slagalica/dao/src/main/java/com/comtrade/repository/skockorepository/SkockoGameRepository.java
+++
@@ -1,9 +0,0 @@
-package com.comtrade.repository.skockorepository;
-
-import com.comtrade.model.skockomodel.SkockoGame;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SkockoGameRepository extends JpaRepository<SkockoGame, Long> {
-}
--- Slagalica/dao/src/main/java/com/comtrade/repository/slagalicarepository/SlagalicaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.comtrade.repository.slagalicarepository;
-
-import com.comtrade.model.slagalicamodel.Slagalica;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SlagalicaRepository extends JpaRepository<Slagalica, Long> {
-}
