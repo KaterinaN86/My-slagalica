8f3ea8305f34ad08b01830b630de524ca45d3f6a
Benjamin Grbo <benjamin.grbo@fsk.unsa.ba>
2022-11-30 10:15:05 +0100

    added logger in service layer

--- service/src/main/java/com/comtrade/service/asocijacijaservice/AsocijacijaServiceImpl.java
+++ service/src/main/java/com/comtrade/service/asocijacijaservice/AsocijacijaServiceImpl.java
@@ -3,7 +3,7 @@ package com.comtrade.service.asocijacijaservice;
 import com.comtrade.model.asocijacijamodel.*;
 import com.comtrade.repository.asocijacijarepository.AsocijacijaRepository;
 import com.comtrade.repository.asocijacijarepository.WordRepository;
-import jakarta.persistence.Index;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 
 @Service
+@Slf4j
 public class AsocijacijaServiceImpl {
     private final AsocijacijaRepository asocijacijaRepository;
     private final WordRepository wordRepository;
@@ -22,41 +23,47 @@ public class AsocijacijaServiceImpl {
 
     private WordModel getRandomWordModel() throws NoSuchElementException{
         int randomId = (int)(Math.floor((Math.random()*wordRepository.count()+1)));
+        log.info("Searching for word model with id: " + randomId);
         Optional<WordModel> randomWordModel = wordRepository.findById(Long.valueOf(randomId));
         if(randomWordModel.isEmpty()){
-            //todo handling - done
-            //todo logging
+            log.info("Word model with id: " + randomId + " does not exists");
             throw new NoSuchElementException();
             //System.out.println("No word model for id: " + randomId);
         }
+        log.info("Returning word model with id: " + randomId);
         return randomWordModel.get();
     }
 
     //creating new Asocijacija game instance
     public ResponseEntity<Response> createNewAsocijacijaGame(){
         try{
+            log.info("Creating new Asocijacija game instance");
             AsocijacijaModel asocijacijaGame = new AsocijacijaModel();
             asocijacijaGame.setWordModel(getRandomWordModel());
             AsocijacijaModel savedAsocijacijaGame = asocijacijaRepository.save(asocijacijaGame);
-
+            log.info("Asocijacija game instance with id: " + savedAsocijacijaGame.getId() + " created.");
             return ResponseEntity.ok()
                     .body(new ResponseWithGameId(savedAsocijacijaGame.getId()));
 
         }catch (NoSuchElementException ex){
+            log.info("Asocijacija game create failed.");
             return ResponseEntity.notFound().build();
         }
     }
 
     //getting optional asocijacija game
     private AsocijacijaModel findSpecificGame(Long gameId) throws NoSuchElementException{
+        log.info("Searching asocijacija game instance with id: " + gameId);
         Optional<AsocijacijaModel> optionalAsocijacijaGame = asocijacijaRepository.findById(gameId);
         if(optionalAsocijacijaGame.isEmpty()){
-            //todo handling
-            //todo logging
+            //todo handling - done
+            //todo logging - done
+            log.info("Asocijacija game instance with id: " + gameId + " not found.");
             throw new NoSuchElementException();
             //System.out.println("No asocijacija game with such id: " + gameId);
             //return null;
         }
+        log.info("Returning asocijacija game instance with id: " + gameId);
         return optionalAsocijacijaGame.get();
     }
 
@@ -68,12 +75,15 @@ public class AsocijacijaServiceImpl {
 
             if(isGameActive && (fieldName.contains("5") || fieldName.contains("final"))){
                 //It does not return value of field A|B|C|D 5(column final word) or final word of a game if it is active
+                log.info("Forbidden request for field:" + fieldName + " - game with id: " + gameId + " still active.");
                 return ResponseEntity.status(403).build();
             }else{
+                log.info("Returning value of field: " + fieldName + " for game id: " + gameId);
                 return ResponseEntity.ok()
                         .body(new ResponseWithFieldValue(findValueOfSpecificCell(findSpecificColumn(asocijacijaGame,fieldNameUpperCase),fieldNameUpperCase)));
             }
         }catch (NoSuchElementException | IndexOutOfBoundsException ex){
+            log.info("Returning value of field: " + fieldName + " for game id: " + gameId + " failed.");
             return ResponseEntity.notFound().build();
         }
     }
@@ -111,8 +121,7 @@ public class AsocijacijaServiceImpl {
         } else if (fieldName.contains("5")) {
             return columnWords.get(4);
         }else{
-            //todo handling - done
-            //todo logging
+            log.info("Field index out of bounds: " + fieldName);
             throw new IndexOutOfBoundsException();
         }
     }
@@ -126,6 +135,7 @@ public class AsocijacijaServiceImpl {
             AsocijacijaModel asocijacijaGame = findSpecificGame(gameId);
             String submittedWordUpperCase = submittedWord.toUpperCase();
             String referenceWordUpperCase = findValueOfSpecificField(asocijacijaGame,fieldName).toUpperCase();
+            log.info("Checking submit for game id: " + gameId);
             if(submittedWordUpperCase.equals(referenceWordUpperCase)){
                 return ResponseEntity.ok()
                         .body(new ResponseWithBoolean(true));
