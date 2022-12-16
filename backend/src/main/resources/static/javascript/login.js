655be8f98e99468227af33f59f45223c7c34e61c
Mirko Petrovic <mirkic.kg99@gmail.com>
2022-12-16 14:36:12 +0100

    fixed some mappings

--- backend/src/main/resources/static/javascript/login.js
+++ backend/src/main/resources/static/javascript/login.js
@@ -2,10 +2,11 @@ regbtn=document.getElementById("registerBtn")
 regbtn.onclick=()=>{window.location.href='http://' + window.location.host + '/register'}
 document.addEventListener("DOMContentLoaded", checkError);
 function checkError(){
+    document.getElementById("form").action="http://" + window.location.host + "/login"
     if(window.location.search=="?error"){
         alert("wrong username or password")
     }
-    else if(window.location.search=="?logot"){
+    else if(window.location.search=="?logout"){
     window.location.href='http://' + window.location.host + '/login'
     }
 }
--- backend/target/classes/static/javascript/login.js
+++ backend/target/classes/static/javascript/login.js
@@ -2,6 +2,7 @@ regbtn=document.getElementById("registerBtn")
 regbtn.onclick=()=>{window.location.href='http://' + window.location.host + '/register'}
 document.addEventListener("DOMContentLoaded", checkError);
 function checkError(){
+    document.getElementById("form").action="http://" + window.location.host + "/login"
     if(window.location.search=="?error"){
         alert("wrong username or password")
     }
