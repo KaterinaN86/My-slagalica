4d5784f52c5eb97a7d3730ea35d7fee457d6a6a2
Mirko Petrovic <mirkic.kg99@gmail.com>
2023-01-04 15:50:21 +0100

    removed profiles

--- backend/src/main/resources/db/changelog/sql/02-insert-into-database.sql
+++ backend/src/main/resources/db/changelog/sql/02-insert-into-database.sql
@@ -3,9 +3,8 @@
 
 
 INSERT INTO users VALUES (
-1,1,"$2a$10$Whcr6HD8FI6/TsaH0nqyMuF8nEYkw987FBkcraT3wLBPSJlnE5/te","ROLE_USER","user1"
-)
-;
+1,1,'$2a$10$Whcr6HD8FI6/TsaH0nqyMuF8nEYkw987FBkcraT3wLBPSJlnE5/te','ROLE_USER','user1'
+);
 INSERT INTO word_model (columnA, columnB, columnC, columnD, final_word) VALUES (
 
 'MODNA,FORMULA,REVIJA,SLETETI,PISTA',
