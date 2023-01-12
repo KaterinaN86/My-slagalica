c39498d5961175cdae0cda8955ace1ea403c9830
adis-p-95 <disee99@gmail.com>
2023-01-12 14:56:27 +0100

    Update SlagalicaPage and SlagalicaPageTest

--- test/selenium/java/pages/SlagalicaPage.java
+++ test/selenium/java/pages/SlagalicaPage.java
@@ -1,7 +1,11 @@
 package pages;
 
 import base.TestBase;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.Reporter;
 
 public class SlagalicaPage extends TestBase {
@@ -10,10 +14,70 @@ public class SlagalicaPage extends TestBase {
 
     By stopButtonLocator = new By.ById("stopButton");
 
+    By potvrdiButtonLocator = new By.ById("submitButton");
+
+    By izbrisiButtonLocator = new By.ById("deleteLetter");
+
+    By firstListLocator = new By.ByXPath("//div[@class='pt-5']");
+
+    By secondListLocator = new By.ByXPath("//body/div[@class='container p-5']/div[@class='row d-block text-center']/div[@class='pt-2']");
+
+    By popUpDialog = new By.ByXPath("//div[@class='modal-content']");
+
+    By closeButton = new By.ByXPath("//button[text()='Zatvori']");
+
+
     public void verifyThatStopButtonIsClicked(){
-        driver.findElement((stopButtonLocator)).click();
+        WebElement stop = driver.findElement((stopButtonLocator));
+        Assert.assertTrue(stop.isDisplayed(), "Stop button not displayed!");
         Reporter.log("Stop button is clicked.");
         System.out.println("Stop button is clicked.");
     }
+
+    public void verifyThatPotvrdiButtonIsClicked(){
+        driver.findElement((potvrdiButtonLocator)).click();
+        Reporter.log("Potvrdi button is clicked.");
+        System.out.println("Potvrdi button is clicked.");
+    }
+
+    public void verifyThatIzbrisiButtonIsClicked(){
+        driver.findElement((izbrisiButtonLocator)).click();
+        Reporter.log("Izbrisi button is clicked.");
+        System.out.println("Izbrisi button is clicked.");
+    }
+
+    public SinglePlayerGamePage verifyThatBackButtonIsClicked(){
+        driver.findElement((locators.getBackBtnLoc())).click();
+        Reporter.log("Back button is clicked.");
+        System.out.println("Back button is clicked.");
+        return new SinglePlayerGamePage();
+    }
+
+    public void verifyThatFirstListIsDisplayed(){
+        driver.findElement((firstListLocator)).isDisplayed();
+        Reporter.log("First list is displayed.");
+        System.out.println("First list is displayed.");
+    }
+
+    public void verifyThatSecondListIsDisplayed(){
+        driver.findElement((secondListLocator)).isDisplayed();
+        Reporter.log("First list is displayed.");
+        System.out.println("First list is displayed.");
+    }
+
+    public void verifyPopUpDialog(){
+        driver.findElement((popUpDialog)).isDisplayed();
+        Reporter.log("Dialog is displayed.");
+        System.out.println("Dialog is displayed.");
+    }
+
+    public void verifyCloseButtonIsClicked(){
+        wait.until(ExpectedConditions.elementToBeClickable(closeButton));
+        driver.findElement((closeButton)).click();
+        Reporter.log("Dialog is displayed.");
+        System.out.println("Dialog is displayed.");
+
+    }
+
 
 }
